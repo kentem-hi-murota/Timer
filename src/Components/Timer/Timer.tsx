50650e6506e3b5bf64dfda6f8203b74520629743
hi-murota <hi-murota@kentem.co.jp>
2024-06-25 10:11:08 +0900

    点滅アニメーション追加

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from 'react';
+import { css, keyframes } from '@emotion/react';
 import { TimerHead, TimerMain } from '../index';
 import { bellPlay, timeStringToNumber } from '../utils';
 
 const Timer = () => {
   const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [isNotify, setIsNotify] = useState<boolean>(false);
   const [currentSeconds, setCurrentSeconds] = useState<number>(30);
   const [bellTimes, setBellTimes] = useState<number[]>([...Array(5)].map(() => 0));
 
   useEffect(() => {
+    const timerHandler = () => {
+      bellPlay();
+      setIsNotify(true);
+      setTimeout(() => {
+        setIsNotify(false);
+      }, 500);
+    };
+
     if (isRunning) {
-      if (bellTimes.includes(currentSeconds) || !currentSeconds) bellPlay();
+      if (bellTimes.includes(currentSeconds) || !currentSeconds) {
+        timerHandler();
+      }
       if (!currentSeconds) setIsRunning(false);
       if (currentSeconds > 0) {
         const timeoutId = setTimeout(() => setCurrentSeconds((prev) => prev - 1), 1000);
@@ -36,19 +48,50 @@ const Timer = () => {
   };
 
   return (
-    <div>
-      <TimerHead
-        isRunning={isRunning}
-        currentSeconds={currentSeconds}
-        startTimer={startTimer}
-        pauseTimer={pauseTimer}
-        resetTimer={resetTimer}
-        bellTimes={bellTimes}
-        setBellTime={setBellTime}
-      />
-      <TimerMain setTime={setTime} currentSeconds={currentSeconds} isRunning={isRunning} />
-    </div>
+    <>
+      {isNotify && <div css={notifyStyle}></div>}
+      <div>
+        <TimerHead
+          isRunning={isRunning}
+          currentSeconds={currentSeconds}
+          startTimer={startTimer}
+          pauseTimer={pauseTimer}
+          resetTimer={resetTimer}
+          bellTimes={bellTimes}
+          setBellTime={setBellTime}
+        />
+        <TimerMain setTime={setTime} currentSeconds={currentSeconds} isRunning={isRunning} />
+      </div>
+    </>
   );
 };
+
+const blink = keyframes`
+  0%, 50% {
+    opacity: 0.7;
+  }
+  25%,75% {
+    opacity: 0;
+  }
+`;
+const notifyStyle = css`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: #000;
+  animation: ${blink} 0.6s step-end;
+`;
+
+// const notifyStyle = css({
+//   position: 'absolute',
+//   top: '0',
+//   right: '0',
+//   left: '0',
+//   bottom: '0',
+//   background: '#000',
+//   animation: {blink}
+// });
 
 export default Timer;
